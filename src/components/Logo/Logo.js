6883eacf86dd6a614f915fbec884dd50a8527d73
MMORONTO <t2wycemo@gmail.com>
2021-08-19 04:48:29 -0400

    import logo image. Apply logo styling. display logo.

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
+import cortechs from './cortechs.png';
 import './Logo.css';
 
 const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
-			<Tilt className="Tilt br2 shadow-2" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
-				<div className="Tilt-inner"> 👀 </div>
+			<Tilt className="Tilt br2 shadow-2" options={{ max : 45 }} style={{ height: 150, width: 150 }} >
+				<div className="Tilt-inner pa3"><img style={{paddingTop: '10px'}} alt='logo' src={cortechs}/></div>
 			</Tilt>
 		</div>
 	);
