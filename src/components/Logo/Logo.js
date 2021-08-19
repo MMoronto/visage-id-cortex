b44ad342582c09d3b8e2df8aac42643dfc5c2a8e
MMORONTO <t2wycemo@gmail.com>
2021-08-19 04:21:39 -0400

    import tilt from react-parallax-tilt library for applying animated effect to our logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Tilt from 'react-parallax-tilt';
 
 const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
-			
+			<Tilt className="Tilt" options={{ max : 25 }} style={{ height: 150, width: 150 }} >
+				<div className="Tilt-inner"> 👀 </div>
+			</Tilt>
 		</div>
 	);
 }
