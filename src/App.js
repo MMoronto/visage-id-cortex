33b50f92a71b694d55770ad2cb7f2c4cb988a6d3
MMORONTO <t2wycemo@gmail.com>
2021-08-19 05:47:22 -0400

    import react-particles and apply to background

--- src/App.js
+++ src/App.js
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import Particles from 'react-particles-js';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+const ParticlesOptions = {
+  particles: {
+    line_linked: {
+      shadow: {
+        enable: true,
+        color: "#3CA9D1",
+        blur: 5
+      }
+    }
+  }
+}
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Particles 
+          params={particlesOptions}
+        />
         <Navigation />
         <Logo />
         <Rank />
--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -3,6 +3,12 @@ import React from 'react';
 const Rank = () => {
 	return (
 		<div>
+			<div className='white f3' >
+				{'Mobola, your current leaderboard position is... '}
+			</div>
+			<div className='white f1' >
+				{'#3'}
+			</div>			
 		</div>
 	);
 }
