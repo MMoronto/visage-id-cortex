c9bff8ff389543b21e05ba79c0844b0cc0e04550
MMORONTO <t2wycemo@gmail.com>
2021-08-20 15:13:22 -0400

    additional CSS style adjustments

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import './Logo.css';
 const Logo = () => {
 	return (
 		<div className='ma4 mt0'>
-			<Tilt className="Tilt br2 shadow-2" options={{ max : 45 }} style={{ height: 150, width: 150 }} >
+			<Tilt className="Tilt br3 shadow-2" options={{ max : 45 }} style={{ height: 150, width: 150 }} >
 				<div className="Tilt-inner pa3"><img style={{paddingTop: '10px' }} alt='logo' src={cortechs}/></div>
 			</Tilt>
 		</div>
