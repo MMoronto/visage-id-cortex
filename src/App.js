e8a1fd1bbbf8ac419ab2a5364b8f6e8e96180e8b
MMORONTO <t2wycemo@gmail.com>
2021-08-27 11:47:43 -0400

    Setup state and routes for signin and register state changes

--- src/App.js
+++ src/App.js
@@ -32,7 +32,8 @@ class App extends Component {
       input: '',
       imageUrl: '',
       box: {},
-      route: 'signin'
+      route: 'signin',
+      isSignedIn: 'false'
     }
   }
 
@@ -70,6 +71,11 @@ class App extends Component {
   }
 
   onRouteChange = (route) => {
+    if (route === 'signout') {
+      this.setState({isSignedIn: false})
+    } else if (route === 'home' ) {
+      this.setState({isSignedIn: true})
+    }
     this.setState({route: route});
   }
 
