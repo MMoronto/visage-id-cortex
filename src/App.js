a86caa5019186012ade688c853ffd495bd01f999
MMORONTO <t2wycemo@gmail.com>
2021-09-07 16:18:54 -0400

    set up componentDidMount function to connect react app with server

--- src/App.js
+++ src/App.js
@@ -38,6 +38,11 @@ class App extends Component {
     }
   }
 
+  componentDidMount() {
+    fetch('http://localhost:3000')
+      .then(response => response.json())
+  } 
+
   calculateFaceLocation = (data) => {
     const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
     const image = document.getElementById('inputimage');
