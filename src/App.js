430e86c31261755bb705fc2034d9e2fddfd51685
MMORONTO <t2wycemo@gmail.com>
2021-09-07 16:28:05 -0400

    install corsto enable CORS for our routes

--- src/App.js
+++ src/App.js
@@ -41,6 +41,7 @@ class App extends Component {
   componentDidMount() {
     fetch('http://localhost:3000')
       .then(response => response.json())
+      .then(console.log)
   } 
 
   calculateFaceLocation = (data) => {
