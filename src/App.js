f4d667745a98c03b3f6e8904ac7b2de0ef7bf2f7
MMORONTO <t2wycemo@gmail.com>
2021-09-08 14:57:26 -0400

    pass loadUser into register

--- src/App.js
+++ src/App.js
@@ -116,7 +116,7 @@ class App extends Component {
           : (
               route === 'signin' 
               ? <Signin onRouteChange={this.onRouteChange}/>
-              : <Register onRouteChange={this.onRouteChange}/>
+              : <Register loadUser={this.loadUser} onRouteChange={this.onRouteChange}/>
             )
         }
       </div>
