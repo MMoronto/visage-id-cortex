812f515e22505e773329b7d062d0457d7d737d91
MMORONTO <t2wycemo@gmail.com>
2021-09-08 13:59:28 -0400

    add name, email and password routes

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,6 +1,26 @@
 import React from 'react';
 
 class Register extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			email: '',
+			password: '',
+			name: ''
+		}
+	}
+
+	onNameChange = (event) => {
+		this.setState({name: event.target.value})
+	}
+
+	onEmailChange = (event) => {
+		this.setState({email: event.target.value})
+	}
+
+	onPassWordChange = (event) => {
+		this.setState({password: event.target.value})
+	}
 	render() {
 		return (
 			<article className="br3 ba dark-gray b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
