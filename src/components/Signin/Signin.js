d7fefec587f1f78a567da3ef661c8d95a02c1e49
MMORONTO <t2wycemo@gmail.com>
2021-09-21 14:31:24 -0400

    replace localhost ports with heroku provided endpoints

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
 	}
 
 	onSubmitSignIn = () => {
-		fetch('http://localhost:3000/signin', {
+		fetch('https://aqueous-refuge-22884.herokuapp.com/signin', {
 			method: 'post',
 			headers: {'Content-Type': 'application/json'},
 			body: JSON.stringify({
@@ -29,7 +29,7 @@ class Signin extends React.Component {
 			.then(response => response.json())
 			.then(user => {
 				if (user.id) {
-					this.props.loadUser(user);
+					this.props.loadUser(user)
 					this.props.onRouteChange('home');
 				}
 			})
